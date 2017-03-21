5d0f1a577333c8a103fcd241464b2268530be621
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-20 19:46:54 -1000

    19

--- linkedList.js
+++ linkedList.js
@@ -26,22 +26,68 @@
       head = newNode;
       tail = newNode;
       return newNode;
+    }
+
+    tail.next = newNode;
+    tail = newNode;
+
+  };
+
+
+
+  var remove = function(gaga){
+    // console.log("HEAD",head);
+    // console.log("TAIL",tail);
+    var find =get(gaga);
+
+    var previous = get(gaga - 1);
+    var section = get(gaga + 1 );
+
+
+    if(find === false){
+      console.log("find",find);
+      return false;
+    } else if(section === false){
+      previous.next = null;
+      tail = previous;
+    } else if(gaga===0){
+      head=section;
+    } else if(find === false){
+      console.log("find",find);
+      return false;
+    }
+
+
+
+//edge case if node delete itself
+    // if(previous === false && section === false){
+    //   head = null;
+    //   tail = null;
+    // }
+
+
+
+    previous.next = section;
+
+    console.log("HEAD2",head);
+    console.log("TAIL2",tail);
+    console.log("gaga2",get(2));
+
+  };
+
+  var get = function(dada){
+
+    var currentNode = head;
+    for(var i =0; i < dada;i++){
+      if(currentNode.next === null){
+        return false;
       }
-
-      tail.next = newNode;
-      tail = newNode;
-
+      currentNode = currentNode.next;
+    }
+    return currentNode;
   };
 
 
-
-  var remove = function(){
-
-  };
-
-  var get = function(){
-
-  };
 
   var insert = function(){
 
@@ -55,5 +101,5 @@
     get: get,
     insert: insert
   };
-}
+};
 
